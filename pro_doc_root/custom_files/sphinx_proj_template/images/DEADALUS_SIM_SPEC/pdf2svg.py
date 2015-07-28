c3970296f98bd014440e9660575d190d41d3b401
nerdOmat <ghandi_84@hotmail.com>
2015-07-28 16:38:34 +0200

    Deleted image files in pro_doc_root/custom_files.

--- pro_doc_root/custom_files/sphinx_proj_template/images/DEADALUS_SIM_SPEC/pdf2svg.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python
-
-import os
-
-for files in os.listdir("."):
-	if files.endswith(".pdf"):
-		svgFile = open(files, 'rb')
-		os.system("inkscape --without-gui --file=" + files +  " --export-plain-svg=" + os.path.splitext(files)[0] + ".svg")
